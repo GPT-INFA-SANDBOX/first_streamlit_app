63184a7c470681d1256c09e8200445e7a9e1f22c
GPT-INFA-SANDBOX <129134170+GPT-INFA-SANDBOX@users.noreply.github.com>
2023-05-25 14:24:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('What a lovely world is out there !! ')
+
+streamlit.title('Take a deep breathe !! ')
+
+streamlit.title('Feel yourself and breeze around !! ')
+
+streamlit.title('Everything around is so beautiful !! ')
