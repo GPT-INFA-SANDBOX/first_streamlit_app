041949b125563fa7b970be794db97785da583d6f
GPT-INFA-SANDBOX <129134170+GPT-INFA-SANDBOX@users.noreply.github.com>
2023-05-25 15:11:37 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
 
 streamlit.title('What a lovely world is out there !! ')
-
-streamlit.header('Take a deep breathe !! ')
-
-streamlit.text('Feel yourself and breeze around !! ')
-
-streamlit.text('Everything around is so beautiful !! ')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
